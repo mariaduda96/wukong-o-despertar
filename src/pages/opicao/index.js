b37dbd423085f28198173624c19c3cf2b9e93cef
Maria <mariaeduardaa54980@gmail.com>
2024-09-17 21:43:18 -0300

    pagina opicao

--- src/pages/opicao/index.js
+++ src/pages/opicao/index.js
@@ -1,22 +1,23 @@
 import './index.css';
 
 
-function Opicao() {
+function opicaofora() {
   return (
 
-        <div class="divcentral">
+        <div class="divcentral-opicaofora">
 
            <img className='img-opicao' src='assets/img/fundo-madera.png'/>
 
-           <button className='creditos'> Créditos </button>
-           <button className='opcao'> Opção </button>
-           <button className='voltamenu1'></button>
-           <button className='cancelar1'></button>
-           <button className='duvidas1'></button>
-
+        
+           <button className='creditos-opicaofora'> Créditos </button>
+           <button className='opcao-opicaofora'> Opção </button>
+           <button className='voltamenu1-opicaofora'></button>
+           <button className='cancelar1-opicaofora'></button>
+           <button className='duvidas1-opicaofora'></button>
+           
         </div>
 
   );
 }
 
-export default Opicao;
+export default opicaofora;
